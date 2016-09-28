da3cbb3295ff2079e7611b36d5cd29b5a69ff9e7
Gustavo6046 <gugurehermann@gmail.com>
2016-09-28 20:43:11 -0300

    Added current source code
    A converter that doesn't works very well, isn't it? Whethever, let's do
    this open-sourcing of my C++ project because we need Unreal meshes!

---
+++ 3ds2unr.h
@@ -0,0 +1,30 @@
+#ifndef F3DS2UNR_H
+#define F3DS2UNR_H
+
+#define WIN32_LEAN_AND_MEAN
+#define STRICT  1
+
+#pragma warning (disable:4786) // ignore "debug information truncated..."
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string>
+using std::string;
+
+#include <vector>
+#include <map>
+using std::vector;
+using std::map;
+
+#include <windows.h>
+#include <shlobj.h>
+
+#include "File3DS.h"
+#include "Registry.h"
+#include "UnrealModel.h"
+
+const char Version[] = "1.00 17-Mar-98";
+
+#endif // F3DS2UNR_H
